3d547e2e5ba0e44f874a1efa0b932fc7ce5890b3
Allen <cobby319@163.com>
2023-06-21 15:50:37 +0800

    update

--- doHist.sh
+++ doHist.sh
@@ -7,7 +7,7 @@ right_edge=55
 # Loop while the right edge is less than or equal to 100
 while (( right_edge <= 250 )); do
   # Run the command with the current left and right edges
-  ./bin/makeHisto --input samples/2018/2018AllData.root --output /Users/allen/TnPtree-analyzer/output --left-edge $left_edge --right-edge $right_edge --config config/photonHLT.yaml
+  ./bin/getEff --input input.root --output eff$left_edge.root --left-edge $left_edge --right-edge $right_edge --config config/photonHLT.yaml
 
   # Update the edges for the next iteration
   right_edge=$(( right_edge + 5 ))

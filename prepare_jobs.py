78f811b856327503d955eefdf86e2fe0810d330e
Allen <cobby319@163.com>
2023-06-22 14:28:52 +0800

    update job script

--- prepare_jobs.py
+++ prepare_jobs.py
@@ -3,20 +3,29 @@
 import os
 
 # Set the initial left and right edges
-left_edge = 50
-right_edge = 55
+left_edge = 25
+right_edge = 30
 
-if not os.path.isdir("jobs"):
-    os.mkdir("jobs")
+if len(sys.argv) < 4:
+    print("Usage: python prepare_jobs <input_dir> <output_dir> <config_path>")
+else:
+   input_tmp = sys.argv[1]
+   output_tmp = sys.argv[2]
+   config_tmp = sys.argv[3]
+input_dir = os.path.abspath(input_tmp)
+output_dir = os.path.abspath(output_tmp)
+config_path = os.path.abspath(config_tmp)
+if not os.path.isdir(output_dir):
+    os.mkdir(output_dir)
 current_path = os.getcwd()
 # Loop while the right edge is less than or equal to 100
-while right_edge <= 250:
+while right_edge <= 420:
     # Generate the command with the current left and right edges
-    command = f"{current_path}/bin/getEff --input {current_path}/input.root --output eff{left_edge}.root --left-edge {left_edge} --right-edge {right_edge} --config {current_path}/config/photonHLT.yaml"
+    command = f"{current_path}/bin/getEff --input {input_dir} --output eff{left_edge}.root --left-edge {left_edge} --right-edge {right_edge} --config {config_path}"
 
     # Generate the job script
     job_script = f"""#!/bin/bash
-cd {current_path}/jobs
+cd {output_dir}
 cp {current_path}/env_lcg.sh .
 . env_lcg.sh
 sleep 5
